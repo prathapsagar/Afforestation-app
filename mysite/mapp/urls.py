73b57134068da6fcb5501722681b679af7ced3b2
Vishwas. R <vishwasrvibhu@gmail.com>
2020-10-28 12:03:02 +0530

    Afforestation app using Django

---
+++ mysite/mapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+
+app_name = 'mapp'
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='mapp/index.html')),
+    path('mapp/out',
+         views.MapCreateView.as_view(), name='map'),
+    path('mapp/out2',
+         views.MapCreateView2.as_view(), name='map2'),
+]
